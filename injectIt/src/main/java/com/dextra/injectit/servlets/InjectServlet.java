cb20de1e5e2acdfdb69f6b09fe44356d7f6bc75b
Bruno Mattoso Gonçalves <bruninhomattoso@hotmail.com>
2015-02-06 13:12:57 -0200

    Gson adicionado ao pom do projeto

--- injectIt/src/main/java/com/dextra/injectit/servlets/InjectServlet.java
+++ injectIt/src/main/java/com/dextra/injectit/servlets/InjectServlet.java
@@ -3,14 +3,17 @@ package com.dextra.injectit.servlets;
 import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.xml.ws.Response;
 
 import com.dextra.injectit.database.Database;
+import com.google.gson.Gson;
 
 public class InjectServlet extends HttpServlet {
 
@@ -22,12 +25,8 @@ public class InjectServlet extends HttpServlet {
 		
 		try {
 
-			String q = req.getParameter("name");
-
 			String query = "SELECT * FROM USER";
-			if (q != null) {
-			}
-
+			
 			ResultSet rs = Database.execute(query);
 			resp.setContentType("text/html");
 			ServletOutputStream os = resp.getOutputStream();
@@ -50,5 +49,31 @@ public class InjectServlet extends HttpServlet {
 		} catch (SQLException e1) {
 			throw new ServletException(e1.getMessage());
 		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		
+		// Nome dos usuario pesquisado
+		/*String name = req.getParameter("name");
+		
+		ResultSet users = Database.execute("SELECT * FROM USER WHERE NAME = '" + name + "'");
+		ArrayList<User> searchedUsers = new ArrayList<User>();
+		try {
+			
+			while (users.next()){
+				String userName = users.getString(1);
+				String userPassword = users.getString(2);
+				String userCard = users.getString(3);
+				User user = new User(userName,userPassword,userCard);
+			}
+			
+			Gson gson = new Gson();
+			gson.toJson(src);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}	
+		*/
 	}
 }
