007e4a4572aed4e56a1fb3da456d29f11813edfb
Vinicius Rodrigues Moreti <viniciusr.moreti@gmail.com>
2015-02-10 08:50:08 -0200

    SUBIU APLICACAO AEHOO

--- webapp/src/main/java/com/dextra/webapp/context/ServerProperties.java
+++ webapp/src/main/java/com/dextra/webapp/context/ServerProperties.java
@@ -1,20 +1,27 @@
 package com.dextra.webapp.context;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ServerProperties {
 
     private static final String NAME = "webapp.properties";
     private static final ServerProperties SINGLETON = new ServerProperties();
+    private final Logger logger = LoggerFactory.getLogger(ServerProperties.class);
 
     private final Properties properties;
 
     private ServerProperties() {
         this.properties = new Properties();
+        
         try {
+        	File f = new File(NAME);
             this.properties.load(this.getClass().getClassLoader().getResourceAsStream(NAME));
-        } catch (IOException e) {
+        } catch (Exception e) {
+        	logger.error("LoadProperties", e);
             throw new RuntimeException(e);
         }
     }
